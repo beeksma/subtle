d168ced0355f4b67ff52404e7d75ef12f918bc48
Victor Beek <beeksma@pm.me>
2017-10-05 22:40:24 +0100

    Add basic Flask files and folder structure

---
+++ Subtle.py
@@ -0,0 +1,3 @@
+#!flask/bin/python
+from web import app
+app.run(debug=True)
---
+++ web/views.py
@@ -0,0 +1,7 @@
+from web import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
