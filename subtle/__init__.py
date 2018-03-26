4ea22eee6cb86ec32b4251b73e01e2aa2c9f3b23
Victor Beek <beeksma@pm.me>
2018-03-26 22:16:10 +0100

    Allow enabling of verbose logging via config.json

--- subtle/__init__.py
+++ subtle/__init__.py
@@ -1,6 +1,8 @@
 from subtle.osapi import OSHandler
 from pathlib import Path
 from collections import OrderedDict
+from web import log, wz_log, logger
+import sys
 import json
 import hashlib
 
@@ -9,22 +11,39 @@ config_exists = Path("config.json").is_file()
 settings = None
 
 if config_exists:
-    with open('config.json', 'r') as json_settings:
-        settings = json.load(json_settings, object_pairs_hook=OrderedDict)
-    if not settings["os_username"] and \
-       (not settings["os_password"] or not settings["hash"]):
-        raise ValueError(
-            'OpenSubtitles credentials have not been set in config.json!')
-    os_handler.user_name = settings["os_username"]
+    try:
+        with open('config.json', 'r') as json_settings:
+            settings = json.load(json_settings, object_pairs_hook=OrderedDict)
 
-    if settings["os_password"]:
-        os_handler.hash = hashlib.md5(settings["os_password"]
-                                      .encode('utf-8')).hexdigest()
-        settings["os_password"] = ""
-        settings["hash"] = os_handler.hash
-        with open('config.json', 'w') as json_settings:
-            json.dump(settings, json_settings, indent=4)
-    else:
-        os_handler.hash = settings["hash"]
+        # Get OS credentials
+        if not settings["os_username"] and \
+           (not settings["os_password"] or not settings["hash"]):
+            raise ValueError(
+                'OpenSubtitles credentials have not been set in config.json!')
+        os_handler.user_name = settings["os_username"]
+
+        # Set hash if OS password is not blank
+        if settings["os_password"]:
+            os_handler.hash = hashlib.md5(settings["os_password"]
+                                          .encode('utf-8')).hexdigest()
+            settings["os_password"] = ""
+            settings["hash"] = os_handler.hash
+            with open('config.json', 'w') as json_settings:
+                json.dump(settings, json_settings, indent=4)
+        else:
+            os_handler.hash = settings["hash"]
+
+        # Enable verbose logging if debugging enabled
+        if settings["debug"].lower() == 'yes':
+            logger.setLevel(log.DEBUG)
+            wz_log.setLevel(log.DEBUG)
+
+    except Exception as e:
+        log.error(e.args[0])
+        sys.exit(1)
+else:
+    raise ValueError("ERROR: 'config.json' is missing in root folder."
+                     " Copy 'config.json.sample' to 'config.json', "
+                     "add your OpenSubtitles credentials, and try again!")
 
 os_handler.login()
--- web/__init__.py
+++ web/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from web.types import Navigator
 import logging as log
 
+# Set up logging
 wz_log = log.getLogger('werkzeug')
 wz_log.setLevel(log.ERROR)
-log.basicConfig(level=log.DEBUG, filename="subtle.log", filemode="a+",
+log.basicConfig(level=log.WARN, filename="subtle.log", filemode="a+",
                 format="%(asctime)-15s %(levelname)-8s %(message)s")
+logger = log.getLogger()
 
+# Run Subtle
 app = Flask(__name__)
 app.config.from_object('flaskconf')
 navigator = Navigator()
